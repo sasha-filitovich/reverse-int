1ea5e15160442bbecfa53100bf92a02e3fbff509
sasha-filitovich <sasha.filitovich@mail.ru>
2023-01-10 14:24:58 +0300

    feat: add the problem solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  const str = String (n);
+  const positive = str.replace (/-/,'');
+  let result = '';
+  for (let i=0; i<positive.length; i++) {
+    result = `${positive[i]}${result}`;
+  }
+  return Number(result);
 }
